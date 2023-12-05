9d62445fe5c76a8a2ad982a7df22dcebcc2872b6
franedsandi <franedsandi@gmail.com>
2023-12-05 12:07:57 +0100

    [feat] projects with an specific technology

--- resources/views/admin/technologies/project-technology.blade.php
+++ resources/views/admin/technologies/project-technology.blade.php
@@ -1,4 +1,11 @@
 @extends('layouts.admin')
 @section('content')
 <h1>projets with {{$technology->name}}</h1>
+
+<ul>
+    @foreach ($technology->projects as $project )
+        <li><a href="{{route('admin.projects.show', $project)}}">{{$project->title}}</a></li>
+    @endforeach
+    
+</ul>
 @endsection
